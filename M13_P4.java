e5c3861f982e783fd6a881ddac4dae6a34a44a27
Arcofe <arcofe@bemen3.cat>
2021-04-14 19:49:31 +0200

    Funcion Suma Resta

--- M13_P4.java
+++ M13_P4.java
@@ -30,5 +30,14 @@ public class Hello {
 
 
         }
-
+		
+	public static int add(int x, int y){
+        int result = x + y;
+        return result;
+    }
+	
+    public static int sub(int x, int y){
+        int result = x-y;
+        return result;
+    }
 }
